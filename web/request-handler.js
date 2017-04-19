083559773006a7f2a50a57b91d74ce796141cf9a
Hack Reactor Student <student@hackreactor.com>
2017-04-19 15:50:12 -0700

    pass one test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var jacksonIsAGoldenGod = require('./http-helpers');
+
+
+var sickAssCallback = 
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === '/') {
+	  jacksonIsAGoldenGod.serveAssets(res, archive.paths.siteAssets + '/index.html', function(err, content) {
+      if (err) {
+        res.writeHead(500);
+        res.end();
+      } else {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content, "utf-8");
+      }
+    })
+  } 
 };
